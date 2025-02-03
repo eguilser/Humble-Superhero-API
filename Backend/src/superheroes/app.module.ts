e65d881e939d38f8893591153766ec0ac2947485
Sergio <sergioeguiluz614@gmail.com>
2025-02-03 15:46:21 +0100

    Delete Backend/src/superheroes/app.module.ts

--- Backend/src/superheroes/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SuperheroesModule } from './superheroes/superheroes.module';
-
-@Module({
-  imports: [SuperheroesModule],
-})
-export class AppModule {}
